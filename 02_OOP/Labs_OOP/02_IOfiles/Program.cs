c2feba0e81cb474cfe10d44d057772795c3a772a
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-02-06 23:30:09 +0200

    add 2 labs

---
+++ 02_OOP/Labs_OOP/02_IOfiles/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_IOfiles
+{
+//    З клавіатури вводиться прізвище, ім’я і розмір
+//    депозиту, а у файл виводиться прізвище, ім’я і розмір
+//    депозиту за 1, 3, 6 і 12 місяці з урахуванням ставки по
+//    депозитам на рівні 4% на місяць
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int size = Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText("result.txt",
+                String.Format($"{name}\nDeposit amount - {size}\n1 month - {CountProfit(size, 1, 4)}\n3 month - {CountProfit(size, 3, 4)}\n6 month - {CountProfit(size, 6, 4)}\n12 month - {CountProfit(size, 12, 4)}"));
+            Console.WriteLine(CountProfit(size, 1, 4));
+            Console.WriteLine(CountProfit(size, 3, 4));
+            Console.WriteLine(CountProfit(size, 6, 4));
+            Console.WriteLine(CountProfit(size, 12, 4));
+            //Console.WriteLine(CountProfit(size, 12, 4, true));
+        }
+
+        // bool adding 
+        // by default your profit will transfer to your payment card. and you will get fixed rate for every month;
+        // but if you add you profit from every month to your deposit, you will gain more money.
+        public static double CountProfit(int depSize, int month, int percent, bool adding = false)
+        {
+            if (depSize <= 0)
+                return 0;
+            else if (!adding)
+                return depSize + month * (depSize * percent / 100);
+            else
+            {
+                for (int i = 0; i < month; i++)
+                {
+                    depSize += (depSize * percent / 100);
+                }
+                return depSize;
+            }
+
+        }
+    }
+}
--- 02_OOP/Labs_OOP/Labs_OOP/Program.cs
+++ 02_OOP/Labs_OOP/Labs_OOP/Program.cs
@@ -6,10 +6,108 @@ using System.Threading.Tasks;
 
 namespace Labs_OOP
 {
+    //    Написати реалізацію перевантажених функцій:
+    //    int func(double* arr, int length);
+    //    int func(char* str);
+    //    Функція func повертає кількість елементів, які більші за своїх сусідів.
     class Program
     {
+
+        const string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+        
+
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int length = rnd.Next(5, 15);
+            double[] arr = new double[length];
+            char[] str = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = (double)rnd.Next(-100, 100) + (double)rnd.Next(100) / 100;
+                str[i] = chars[rnd.Next(0, chars.Length - 1)];
+            }
+            foreach (var el in arr)
+            {
+                Console.Write($"{el} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"There are {func(arr, length)} elements that are bigger than their neighbours.");
+            Console.WriteLine();
+            Console.WriteLine(str);
+            Console.WriteLine($"There are {func(str)} elements that are bigger than their neighbours.");
+            Console.WriteLine("Using ASCII Table to compare. ");
+            Console.WriteLine();
+
+
+        }
+        public static int func(double[] arr, int length)
+        {
+            if (arr.Length == 0)
+                return 0;
+            else if (arr.Length != length)
+                throw new ArgumentException("Length isn`t correct.");
+            else
+            {
+                int counter = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    if (i == 0)
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            counter++;
+                        }
+
+                    }
+                    else if (i == length - 1)
+                    {
+                        if (arr[i] > arr[i - 1])
+                        {
+                            counter++;
+
+                        }
+                    }
+                    else if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+                        counter++;
+                }
+                return counter;
+            }
+        }
+        public static int func(char[] str)
+        {
+            //string str = str.ToString();
+            if (str.Length == 0)
+               return 0;
+            else
+            {
+                int length = str.Length;
+                int counter = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    if (i == 0)
+                    {
+                        if ((int)str[i] > (int)str[i + 1])
+                        {
+                            counter++;
+                        }
+
+                    }
+                    else if (i == length - 1)
+                    {
+                        if ((int)str[i] > (int)str[i - 1])
+                        {
+                            counter++;
+
+                        }
+                    }
+                    else if (str[i] > (int)str[i - 1] && (int)str[i] > (int)str[i + 1])
+                        counter++;
+                }
+                return counter;
+            }
+
         }
     }
 }
